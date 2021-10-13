6bf7c208d423338a5d6d9a3e1282de4c3863d67f
Jake May <jakemaydev@gmail.com>
2021-10-13 14:43:02 +1100

    very simple working version using user input via cli

---
+++ pi-estimation/src/App.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        piEstimation();
+    }
+
+    public static void piEstimation(){
+        Scanner scan = new Scanner(System.in);
+
+        double a = 1;
+        double aNext = a;
+        double b = 1/Math.sqrt(2);
+        double t = 1/4.0;
+        int p = 1;
+
+        System.out.println("How many iterations?");
+        int iterations = scan.nextInt();
+
+        for(int i = 0; i < iterations; i++){
+            aNext = (a+b)/2;
+            b = Math.sqrt(a*b);
+            t = t - p*Math.pow((a - aNext), 2);
+            p = 2*p;
+            a = aNext;
+        }
+
+        System.out.println((Math.pow(aNext + b, 2)) / (4*t));
+    }
+}
+
